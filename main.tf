42b018220ea34ace6ebde6b3c808cbc184e4a52b
Ibarra <g.ibarra@dxc.com>
2022-01-11 22:01:54 -0600

    3do

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ terraform {
 }
 
 provider "digitalocean" {
-  token = "69d5b83f950d93d34dfe5859c0db88f8657e204e416d003818a3f3aa3d062030"
+  token = "token"
 }
 
 resource "digitalocean_droplet" "web" {
   image  = "ubuntu-20-04-x64"
-  name   = "ubuntu20-test"
+  name   = "ubuntu20-test2"
   region = "nyc3"
   size   = "s-1vcpu-1gb"
   ssh_keys = ["f1:1a:b4:75:75:c8:8d:b3:ed:9c:e6:15:1e:4d:2f:53"]
